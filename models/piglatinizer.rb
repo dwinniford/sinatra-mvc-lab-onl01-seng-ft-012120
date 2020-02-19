c1f8c7da830e3ab93387d67a0dbcd185cb4766f7
Dave Winniford <dwinniford@gmail.com>
2020-02-19 15:10:00 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,3 +1,21 @@
 class PigLatinizer
-
+  attr_accessor :user_text
+  
+  def initialize(user_text)
+    @user_text= user_text
+  end 
+  
+  def text_to_arr
+  end 
+  
+  def piglatinize_word(word)
+    if word.split.first.match(/[aeiou]/)
+      word + "way"
+    else
+      
+    end 
+  end 
+  
+  def piglatinize_text
+  end 
 end 
